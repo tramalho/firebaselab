0969372b1405077fb6f5ea46455e6d2252d5d77b
tramalho <tramalho@ciandt.com>
2018-06-22 09:54:32 -0300

    - Receive Message - Subscribe topic - PopUp Notification

--- app/src/main/java/com/example/tramalho/firebaselab/FirebaseInstanceIdServiceImp.kt
+++ app/src/main/java/com/example/tramalho/firebaselab/FirebaseInstanceIdServiceImp.kt
@@ -3,7 +3,7 @@ package com.example.tramalho.firebaselab
 import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceIdService
 import com.google.firebase.iid.FirebaseInstanceId
-
+import com.google.firebase.messaging.FirebaseMessaging
 
 
 /**
@@ -16,8 +16,12 @@ class FirebaseInstanceIdServiceImp : FirebaseInstanceIdService() {
 
         val token = FirebaseInstanceId.getInstance().token
 
+        //deve existir o mesmo topico no FCM
+        FirebaseMessaging.getInstance().subscribeToTopic("fcm_topic")
+
         //nesse ponto realizar chamadas para quaisquer interessados pelo
         //token unico
+        //TODO
 
         Log.d(javaClass.simpleName, "Novo token ${token}")
     }
